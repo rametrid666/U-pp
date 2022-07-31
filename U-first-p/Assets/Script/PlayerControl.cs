a98f374306cc66990f848dc028aa973b6e6e7437
rametrid666 <C110152330@nkust.edu.tw>
2022-07-31 16:45:26 +0800

    Update some anim
    meow

--- U-first-p/Assets/Script/PlayerControl.cs
+++ U-first-p/Assets/Script/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PlayerControl : MonoBehaviour
     [SerializeReference] float speed = 5f;
     [SerializeReference] SpriteRenderer sprite;
     [SerializeReference] Animator anim;
+    
     void Start()
     {
         
@@ -17,41 +19,48 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         Move();
-        
+
     }
     private void FixedUpdate()
     {
-       
+        
     }
     void Move() 
     {
-        if (Input.GetKey(KeyCode.RightArrow)) 
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            rigi.velocity += new Vector2(speed*Time.deltaTime,0f);
-            sprite.flipX = false;
-            //anim.SetBool("isRun", true);
+            rigi.velocity += new Vector2(speed * Time.deltaTime, 0f);
+            if (rigi.velocity.x > 1f)
+            { 
+                sprite.flipX = false;
+                anim.SetBool("isRun", true);                
+            }
 
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             rigi.velocity -= new Vector2(speed * Time.deltaTime, 0f);
-            sprite.flipX = true;
-            //anim.SetBool("isRun", true);
-        }
-        if (rigi.velocity == new Vector2(0f, 0)) 
-        {
-            anim.SetBool("isRun", false);
-        }
-        else 
-        {
-            anim.SetBool("isRun", true);
-
-        }
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) 
-        {
+            if(rigi.velocity.x < -1f) 
+            {
+                sprite.flipX = true;
+                anim.SetBool("isRun", true);
+            }
             
         }
 
+        if (rigi.velocity == new Vector2(0f,0f) && anim.GetCurrentAnimatorStateInfo(0).IsName("RPGboy_walk"))
+        {
+            anim.SetBool("isRun", false);
+        }        
 
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            anim.SetBool("isGround", false);
+            rigi.AddForce(new Vector2(0f, 10f),ForceMode2D.Impulse);          
+        }
+        Debug.Log(rigi.velocity.x);
     }
+
+   
+
 }
